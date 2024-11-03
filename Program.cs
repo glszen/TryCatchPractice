f7e305757a863ae6d5a3be518ab6229ff1292a91
Gülşen <gulsenk48@hotmail.com>
2024-11-03 14:20:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Start:
+try 
+{
+    Console.Write("Please enter a number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int square = number * number;
+
+    Console.WriteLine("Square in number is: " + square);
+
+}
+catch (FormatException) //Error type specified, if the user enters an incorrect format they will encounter an error
+{
+    Console.WriteLine("Error! Please enter a valid number."); 
+    goto Start; //Reverted to receive new input from the user
+}
